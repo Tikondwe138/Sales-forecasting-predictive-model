e43bafd4d3f10ed43a2906e892c43b3d895f169f
Tikondwe Mathias  Kaonga <128364919+Tikondwe138@users.noreply.github.com>
2025-05-30 01:52:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-main.py
+from src.preprocessing import load_and_prepare_data
+from src.forecasting_arima import run_arima
+from src.forecasting_prophet import run_prophet
+from src.evaluation import evaluate_model
+
+# Load + clean
+df = load_and_prepare_data("data/sales_data.csv")
+
+# Run ARIMA
+arima_forecast = run_arima(df)
+evaluate_model(df, arima_forecast, model_name="ARIMA")
+
+# Run Prophet
+prophet_forecast = run_prophet(df)
+evaluate_model(df, prophet_forecast, model_name="Prophet")
